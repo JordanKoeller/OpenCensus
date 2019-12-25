1e65349039016e9c3b47721a65d14629f5969553
Jordan Koeller <jkoeller12@gmail.com>
2019-12-25 09:46:27 -0600

    decreased package size. Added venv usage for building deployments

--- deploy-all-lambdas.sh
+++ deploy-all-lambdas.sh
@@ -2,7 +2,18 @@ cd lambdas
 for d in $(ls -1)
 do
 	cd $d
-	npm install
+	if [ -f setup.py ]
+	then
+		python3 -m venv venv
+		source venv/bin/activate
+		pip install -r dev-requirements.txt
+		python setup.py build_ext --inplace
+		deactivate
+	fi
+	if [ -f package.json ]
+	then
+	        npm install
+        fi
 	serverless deploy
 	cd ..
 done
