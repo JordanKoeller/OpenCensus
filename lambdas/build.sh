0b5b633be88df828c71e2ebf21185fecb883e3af
Jordan Koeller <jkoeller12@gmail.com>
2020-02-08 15:47:09 -0600

    Fixing deploy scripts.

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -6,6 +6,7 @@ then
   rm -rf build *.c *.cpythonc *.so
   pip install numpy==1.15.4 cython
   python3 setup.py build_ext --inplace
+  cd ..
 else
 	echo "With container"
 	docker run --rm -it -w "/build-pkg" -v "$PWD/app/:/build-pkg" lambci/lambda:build-python3.7 bash -c "rm -rf build *.c *.cpythonc *.so && pip install numpy==1.15.4 cython && python3 setup.py build_ext --inplace"
